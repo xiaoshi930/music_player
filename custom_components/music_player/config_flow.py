173964b32f6cd7754270a4401eaa054e4ecedd3d
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2025-12-06 17:07:10 +0800

    Update config_flow.py

--- custom_components/music_player/config_flow.py
+++ custom_components/music_player/config_flow.py
@@ -1,18 +1,11 @@
 from __future__ import annotations
 
 import voluptuous as vol
-import sys
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 
 from .const import DOMAIN, CONF_SOURCE, DEFAULT_SOURCE, SOURCES
-
-try:
-    from homeassistant.helpers.typing import ConfigType
-    NEW_HOMEASSISTANT = True
-except ImportError:
-    NEW_HOMEASSISTANT = False
 
 class NeteaseLyricsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
@@ -51,11 +44,8 @@ class NeteaseLyricsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class NeteaseLyricsOptionsFlow(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        if NEW_HOMEASSISTANT:
-            self._config_entry = config_entry  
-            super().__init__()
-        else:
-            self.config_entry = config_entry
+        """Initialize options flow."""
+        self._config_entry = config_entry
 
     async def async_step_init(
         self, user_input=None
@@ -63,16 +53,14 @@ class NeteaseLyricsOptionsFlow(config_entries.OptionsFlow):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        config_entry = self._config_entry if NEW_HOMEASSISTANT else self.config_entry
-
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
                 vol.Required(
                     CONF_SOURCE,
-                    default=config_entry.options.get(  
+                    default=self._config_entry.options.get(  
                         CONF_SOURCE, 
-                        config_entry.data.get(CONF_SOURCE, DEFAULT_SOURCE)
+                        self._config_entry.data.get(CONF_SOURCE, DEFAULT_SOURCE)
                     ),
                 ): vol.In(SOURCES),
             }),
